432e049bbdee7a1625e3e623388c6fbb30c75a03
mthomas <mthomas@37a9785b-7235-40ca-9b3e-c97ac53a81ac>
2011-01-06 21:18:34 +0000

    Revert of change in rev 404
    git-svn-id: http://servebrain/svn/joomla-engineering/branches/je-construct-pro-1-6@427 37a9785b-7235-40ca-9b3e-c97ac53a81ac

--- logic.php
+++ logic.php
@@ -64,51 +64,91 @@ if ($loadMoo) {
 }
 
 #----------------------------- Moldule Counts -----------------------------#
-// from http://forum.joomla.org/viewtopic.php?t=220574
+// from http://groups.google.com/group/joomla-dev-general/browse_thread/thread/b54f3f131dd173d
 
-$supraModuleCount = $this->countModules('supra1 + supra2 + supra3 + supra4', true);
+$supraCount1 = (int) ($this->countModules('supra1') > 0);
+$supraCount2 = (int) ($this->countModules('supra2') > 0);
+$supraCount3 = (int) ($this->countModules('supra3') > 0);
+$supraCount4 = (int) ($this->countModules('supra4') > 0);
+
+$supraModuleCount = $supraCount1 + $supraCount2 + $supraCount3 + $supraCount4;
 
 if ($supraModuleCount) : $supraModuleClass = 'count-'.$supraModuleCount; endif;
 
 #--------------------------------------------------------------------------#
 
-$subHeadCount = $this->countModules('subhead1 + subhead2 + subhead3 + subhead4 + subhead5 + subhead6', true);
+$subHeadCount1 = (int) ($this->countModules('subhead1') > 0);
+$subHeadCount2 = (int) ($this->countModules('subhead2') > 0);
+$subHeadCount3 = (int) ($this->countModules('subhead3') > 0);
+$subHeadCount4 = (int) ($this->countModules('subhead4') > 0);
+$subHeadCount5 = (int) ($this->countModules('subhead5') > 0);
+$subHeadCount6 = (int) ($this->countModules('subhead6') > 0);
+
+$subHeadCount = $subHeadCount1 + $subHeadCount2 + $subHeadCount3 + $subHeadCount4 + $subHeadCount5 + $subHeadCount6;
 
 if ($subHeadCount) : $subHeadClass = 'count-'.$subHeadCount; endif;
 
 #--------------------------------------------------------------------------#
 
-$subNavCount = $this->countModules('user1 + user2 + user3 + user4', true);
+$subNavCount1 = (int) ($this->countModules('user1') > 0);
+$subNavCount2 = (int) ($this->countModules('user2') > 0);
+$subNavCount3 = (int) ($this->countModules('user3') > 0);
+$subNavCount4 = (int) ($this->countModules('user4') > 0);
+
+$subNavCount = $subNavCount1 + $subNavCount2 + $subNavCount3 + $subNavCount4;
 
 if ($subNavCount) : $subNavClass = 'count-'.$subNavCount; endif;
 
 #--------------------------------------------------------------------------#
 
-$contentTopCount = $this->countModules('top1 + top2 + top3 + top4', true);
+$contentTopCount1 = (int) ($this->countModules('top') > 0);
+$contentTopCount2 = (int) ($this->countModules('top2') > 0);
+$contentTopCount3 = (int) ($this->countModules('top3') > 0);
+$contentTopCount4 = (int) ($this->countModules('top4') > 0);
+
+$contentTopCount = $contentTopCount1 + $contentTopCount2 + $contentTopCount3 + $contentTopCount4;
 
 if ($contentTopCount) : $contentTopClass = 'count-'.$contentTopCount; endif;
 
 #--------------------------------------------------------------------------#
 
-$contentBottomCount = $this->countModules('user5 + user6 + user7 + user8', true);
+$contentBottomCount1 = (int) ($this->countModules('user5') > 0);
+$contentBottomCount2 = (int) ($this->countModules('user6') > 0);
+$contentBottomCount3 = (int) ($this->countModules('user7') > 0);
+$contentBottomCount4 = (int) ($this->countModules('user8') > 0);
+
+$contentBottomCount = $contentBottomCount1 + $contentBottomCount2 + $contentBottomCount3 + $contentBottomCount4;
 
 if ($contentBottomCount) : $contentBottomClass = 'count-'.$contentBottomCount; endif;
 
 #--------------------------------------------------------------------------#
 
-$contentLeftCount = $this->countModules('left + left2', true);
+$contentLeftCount1 = (int) ($this->countModules('left') > 0);
+$contentLeftCount2 = (int) ($this->countModules('left2') > 0);
+
+$contentLeftCount = $contentLeftCount1 + $contentLeftCount2;
 
 if ($contentLeftCount) : $contentLeftClass = 'count-'.$contentLeftCount; endif;
 
 #--------------------------------------------------------------------------#
 
-$contentRightCount = $this->countModules('right + right2', true);
+$contentRightCount1 = (int) ($this->countModules('right') > 0);
+$contentRightCount2 = (int) ($this->countModules('right2') > 0);
+
+$contentRightCount = $contentRightCount1 + $contentRightCount2;
 
 if ($contentRightCount) : $contentRightClass = 'count-'.$contentRightCount; endif;
 
 #--------------------------------------------------------------------------#
 
-$subContentCount = $this->countModules('sub1 + sub2 + sub3 + sub4 + sub5 + sub6', true);
+$subContentCount1 = (int) ($this->countModules('sub1') > 0);
+$subContentCount2 = (int) ($this->countModules('sub2') > 0);
+$subContentCount3 = (int) ($this->countModules('sub3') > 0);
+$subContentCount4 = (int) ($this->countModules('sub4') > 0);
+$subContentCount5 = (int) ($this->countModules('sub5') > 0);
+$subContentCount6 = (int) ($this->countModules('sub6') > 0);
+
+$subContentCount = $subContentCount1 + $subContentCount2 + $subContentCount3 + $subContentCount4 + $subContentCount5 + $subContentCount6;
 
 if ($subContentCount) : $subContentClass = 'count-'.$subContentCount; endif;
 
