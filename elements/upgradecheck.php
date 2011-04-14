202f847cf156de4e256e4e77a4370605868bc2b1
Matt Thomas <matt@betweenbrain.com>
2011-04-14 15:46:00 -0400

    Release pre-flight. Moved CSS parent menu item indicator from screen.css to example.css

--- elements/upgradecheck.php
+++ elements/upgradecheck.php
@@ -21,7 +21,7 @@ class JFormFieldUpgradecheck extends JFormField {
 		if(!function_exists("curl_init")) return 'cURL is not supported by your server. Please contact your hosting provider to enable this capability.';
 		//If cURL is supported, check the current version available.
 		else 
-				$version = 633;
+				$version = 641;
 				$target = 'http://joomlaengineering.com/upgradecheck/je-construct-pro-1-6';
 				$curl = curl_init();
 				curl_setopt($curl, CURLOPT_URL, $target);
@@ -34,7 +34,7 @@ class JFormFieldUpgradecheck extends JFormField {
 				
 				//If the current version is out of date, notify the user and provide a download link.
 				if ($version < $str)
-					$message = $message . '<a href="http://joomlaengineering.com" target="_blank">Version 1.6.'.$str.' is now available.</a></label>';
+					$message = $message . '<a href="http://joomlaengineering.com" target="_blank">Version 1.6.'.$str.' is now available.</a><br /><a href="http://joomlaengineering.com/construct-pro-changelog-1-6" target="_blank">See what&rsquo;s new.</a></label>';
 				//If the current version is up to date, notify the user. 	
 				elseif (($version == $str) || ($version > $str))
 					$message = $message . 'There are no updates available at this time.</label>';
