d4fba7b88a81288b76b5685100952fc4c7fef196
Matt Thomas <matt@betweenbrain.com>
2011-05-02 11:28:55 -0400

    Refactored modeuls.php

--- html/modules.php
+++ html/modules.php
@@ -1,47 +1,45 @@
 <?php defined('_JEXEC') or die;
 /**
-* @package		Template Framework for Joomla! 1.6
+* @package		Template Framework for Joomla! 1.5
 * @author		Joomla Engineering http://joomlaengineering.com
-* @copyright	Copyright (C) 2010, 2011 Matt Thomas | Joomla Engineering. All rights reserved.
+* @copyright	Copyright (C) 2010, 2011 Matt Thomas. All rights reserved.
 * @license		GNU/GPL v2 or later http://www.gnu.org/licenses/gpl-2.0.html
 */
-?>
-<?php function modChrome_jexhtml( $module, &$params, &$attribs ) {
-	
-	$headerLevel = isset($attribs['level']) ? (int) $attribs['level'] : 3;
-	$headerClass = isset($attribs['header-class']) ? $attribs['header-class'] : 'je-header';
-	$moduleClass = isset($attribs['module-class']) ? $attribs['module-class'] : null;
-	if (!empty($module->content)){ ?>
-		<div class="moduletable<?php echo $params->get('moduleclass_sfx'); ?> <?php if($moduleClass) echo $moduleClass; ?>">		
-			<?php if ($module->showtitle) : ?>
-				<h<?php echo $headerLevel; ?> class="<?php echo $headerClass; ?>"><?php echo $module->title; ?><?php echo '</h'.$headerLevel; ?>>
-			<?php endif; ?>
-			<?php echo $module->content; ?>	
-		</div>						
 
-<?php	}
-	}
-?>	
-	
-<?php function modChrome_jerounded( $module, &$params, &$attribs ) {
+function modChrome_jexhtml( $module, &$params, &$attribs ) {
 	$headerLevel = isset($attribs['level']) ? (int) $attribs['level'] : 3;
 	$headerClass = isset($attribs['header-class']) ? $attribs['header-class'] : 'je-header';
-	$moduleClass = isset($attribs['module-class']) ? $attribs['module-class'] : null;
-	if (!empty($module->content))
-		{ ?>
-		<div class="moduletable<?php echo $params->get('moduleclass_sfx'); ?> <?php if($moduleClass) echo $moduleClass; ?>">
+	$moduleClass = isset($attribs['module-class']) ? $attribs['module-class'] : NULL;
+	$outlineStyle = isset($attribs['outline-style']) ? $attribs['outline-style'] : NULL;
+	
+	if (!empty($module->content)) { 
+		echo '<div class="moduletable'.$params->get('moduleclass_sfx'); if( $moduleClass ) echo ' '.$moduleClass; if( $outlineStyle ) echo ' outline-'.$outlineStyle; echo ' clearfix">';
+			if ($module->showtitle) { 
+				echo '<h'.$headerLevel.' class="'.$headerClass.'">'.$module->title.'</h'.$headerLevel.'>';
+			}
+			echo $module->content .'
+		</div>';
+	}
+}	
+
+function modChrome_jerounded( $module, &$params, &$attribs ) {
+	$headerLevel = isset($attribs['level']) ? (int) $attribs['level'] : 3;
+	$headerClass = isset($attribs['header-class']) ? $attribs['header-class'] : 'je-header';
+	$moduleClass = isset($attribs['module-class']) ? $attribs['module-class'] : NULL;
+	$roundedStyle = isset($attribs['rounded-style']) ? $attribs['rounded-style'] : NULL;
+	
+	if (!empty($module->content)) {
+		echo '<div class="moduletable'.$params->get('moduleclass_sfx'); if( $moduleClass ) echo ' '.$moduleClass; if( $roundedStyle ) echo ' rounded-'.$roundedStyle; echo '">
 			<div>
 				<div>
-					<div>		 
-					<?php if ($module->showtitle) : ?>
-						<h<?php echo $headerLevel; ?> class="<?php echo $headerClass; ?>"><?php echo $module->title; ?><?php echo '</h'.$headerLevel; ?>>
-					<?php endif; ?>
-					<?php echo $module->content; ?>
+					<div>';		 
+					if ($module->showtitle) {
+						echo '<h'.$headerLevel.' class="'.$headerClass.'">'.$module->title.'</h'.$headerLevel.'>';
+					}
+					echo $module->content .'
 					</div>
 				</div>
 			</div>
-		</div>						
-
-<?php	}
-	}			
-?>
+		</div>';
+	}
+}
