f691d48f9f6da9559e3f0de2aaa7ea81718c5000
Matt Thomas <matt@betweenbrain.com>
2011-06-13 11:29:25 -0400

    Removed nested category diagnostic output

--- layouts/offline.php
+++ layouts/offline.php
@@ -74,17 +74,6 @@
 						<?php if($itemId)		echo '<li>item-'.$itemId.'</li>'; ?>
 						<?php if($catId)		echo '<li>category-'.$catId.'</li>'; ?>
 						<?php if($sectionId) 	echo '<li>section-'.$sectionId.'</li>'; ?>
-						<?php if ($isOnward && $catId) {							
-								echo '<li>Parent Category '.$parentCategory.'</li>';							
-								echo '<li>Ancestor Categories:';		
-								$results = getAncestorCategories($catId);
-									if (count($results) > 0) {
-										foreach ($results as $item) {
-											echo ' '.$item->id.',';
-										}			
-									}
-								echo'</li>';
-								} ?>
 					</ul>
 				<?php endif; ?>	
 				
