91be08b1eeb23eac3f1a139c06c71ef4f013ab77
Matt Thomas <matt@betweenbrain.com>
2011-08-05 10:24:03 -0400

    Changed exmaple modal artile ID to 1

--- html/mod_footer/default.php
+++ html/mod_footer/default.php
@@ -10,4 +10,4 @@
 $app = JFactory::getApplication();
 ?>
 <p id="siteinfo-legal">All rights reserved. &copy; <?php echo $cur_year ?> <a href="<?php echo $this->baseurl ?>/" title="<?php echo $app->getCfg('sitename');?>"><?php echo $app->getCfg('sitename');?></a>.<br/>
-Developed using the <a href="http://joomlaengineering.com">Construct</a> Template Development Framework.<br/>Joomla! is a registered trademark of Open Source Matters, Inc. - <a class="modal" href="index.php?option=com_content&view=article&id=30&tmpl=modal" rel="{handler: 'iframe', size: {x: 640, y: 480}}">Disclaimer</a></p>
+Developed using the <a href="http://joomlaengineering.com">Construct</a> Template Development Framework.<br/>Joomla! is a registered trademark of Open Source Matters, Inc. - <a class="modal" href="index.php?option=com_content&view=article&id=1&tmpl=modal" rel="{handler: 'iframe', size: {x: 640, y: 480}}">Disclaimer</a></p>
