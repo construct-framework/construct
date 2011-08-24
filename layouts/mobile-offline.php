fbad53c8d71ddeec979d0a4b4a6ddd4c7bd006f2
Matt Thomas <matt@betweenbrain.com>
2011-08-24 09:15:36 -0400

    Updating to jQuery 1.6.2 and jQuery Mobile Beta 2.

--- layouts/mobile-offline.php
+++ layouts/mobile-offline.php
@@ -13,8 +13,8 @@
 		<meta http-equiv="Content-Type" content="<?php echo $contenttype; ?>; charset=utf-8" />
 		<link rel="stylesheet" href="<?php echo $baseUrl.'templates/'.$this->template; ?>/css/mobile.css" type="text/css" media="screen" />
 		<link rel="stylesheet" href="http://code.jquery.com/mobile/1.0a4.1/jquery.mobile-1.0a4.1.min.css" />
-		<script src="http://code.jquery.com/jquery-1.5.2.min.js"></script>
-		<script src="http://code.jquery.com/mobile/1.0a4.1/jquery.mobile-1.0a4.1.min.js"></script>
+		<script src="http://code.jquery.com/jquery-1.6.2.min.js"></script>
+		<script src="http://code.jquery.com/mobile/1.0b2/jquery.mobile-1.0b2.min.js"></script>
 		<script>
 			$(document).ready(function() {
 				$('body').removeClass("noscript");
--- layouts/mobile.php
+++ layouts/mobile.php
@@ -22,8 +22,8 @@ $baseUrl 				= JURI::base();
 			<link rel="stylesheet" href="<?php echo $baseUrl.$mobileCssFile; ?>" type="text/css" media="screen" />			
 		<?php endif; ?>	
 		<link rel="stylesheet" href="http://code.jquery.com/mobile/1.0a4.1/jquery.mobile-1.0a4.1.min.css" />
-		<script src="http://code.jquery.com/jquery-1.5.2.min.js"></script>
-		<script src="http://code.jquery.com/mobile/1.0a4.1/jquery.mobile-1.0a4.1.min.js"></script>
+		<script src="http://code.jquery.com/jquery-1.6.2.min.js"></script>
+		<script src="http://code.jquery.com/mobile/1.0b2/jquery.mobile-1.0b2.min.js"></script>
 		<script>
 			$(document).ready(function() {
 				$('body').removeClass("noscript");
