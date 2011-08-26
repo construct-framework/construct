2c04af86c23271abde557854d68944146f9e73a3
Matt Thomas <matt@betweenbrain.com>
2011-08-26 12:53:01 -0400

    Updating Goolge Web Fonts and jQuery Mobile to Beta 2.

--- layouts/mobile.php
+++ layouts/mobile.php
@@ -21,7 +21,7 @@ $baseUrl 				= JURI::base();
 		if (isset($mobileCssFile)) : ?>
 			<link rel="stylesheet" href="<?php echo $baseUrl.$mobileCssFile; ?>" type="text/css" media="screen" />			
 		<?php endif; ?>	
-		<link rel="stylesheet" href="http://code.jquery.com/mobile/1.0a4.1/jquery.mobile-1.0a4.1.min.css" />
+		<link rel="stylesheet" href="http://code.jquery.com/mobile/1.0b2/jquery.mobile-1.0b2.min.css" />
 		<script src="http://code.jquery.com/jquery-1.6.2.min.js"></script>
 		<script src="http://code.jquery.com/mobile/1.0b2/jquery.mobile-1.0b2.min.js"></script>
 		<script>
