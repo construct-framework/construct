77c9fba892d2ceacef6f24597ccccfbc65b2fdf3
Matt Thomas <matt@betweenbrain.com>
2011-12-13 11:34:37 -0500

    Updating Google Web Font, correcting a few comment blocks

--- elements/googlewebfont.php
+++ elements/googlewebfont.php
@@ -1,10 +1,9 @@
 <?php defined('_JEXEC') or die;
 /**
-* @package		Unified HTML5 Template Framework for Joomla! 1.5,1.6+
-* @author		Cristina Solana http://nightshiftcreative.com
-* @author		Matt Thomas http://construct-framework.com | http://betweenbrain.com
-* @copyright	Copyright (C) 2009 - 2011 Matt Thomas. All rights reserved.
-* @license		GNU/GPL v2 or later http://www.gnu.org/licenses/gpl-2.0.html
+* @package      Template Framework for Joomla!
+* @author       Matt Thomas http://construct-framework.com
+* @copyright    Copyright (C) 2009 - 2011 Matt Thomas. All rights reserved.
+* @license      GNU/GPL v2 or later http://www.gnu.org/licenses/gpl-2.0.html
 */
 
 class JElementGooglewebfont extends JElement 
