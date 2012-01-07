c6adab464cf4a94471da226f8295861b9491caae
Matt Thomas <matt@betweenbrain.com>
2012-01-07 18:16:45 -0500

    Updating copyright date

--- elements/helper.php
+++ elements/helper.php
@@ -2,7 +2,7 @@
 /**
  * @package     Template Framework for Joomla! 1.6
  * @author      Joomla Engineering http://construct-framework.com
- * @copyright   Copyright (C) 2009 - 2011 Matt Thomas. All rights reserved.
+ * @copyright   Copyright (C) 2009 - 2012 Matt Thomas. All rights reserved.
  * @license		GNU/GPL v2 or later http://www.gnu.org/licenses/gpl-2.0.html
  */
 defined('_JEXEC') or die;
